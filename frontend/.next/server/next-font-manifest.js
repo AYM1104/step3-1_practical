d16b1a7d295e9c94fe3261406283e63ffd5cb1ba
ayu1104 <ak3668134@gmail.com>
2025-01-23 23:32:45 +0900

    no message

--- frontend/.next/server/next-font-manifest.js
+++ frontend/.next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ayu/Tech0/Step3/Step3-1/practical/sample4/frontend/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
