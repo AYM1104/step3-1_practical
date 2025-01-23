c40203b099e919ccf5e0a4d08c679f723f61e8a4
ayu1104 <ak3668134@gmail.com>
2025-01-24 01:24:30 +0900

    Remove cached files and apply .gitignore

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ayu/Tech0/Step3/Step3-1/practical/sample4/frontend/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
